41037afd69ea58780f8322384df9fc62c067f5ca
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-04-13 22:01:07 +0300

    + com + counter of balls label + checking lose + spawn balls for every 8 seconds

--- WinForms/Forms/GDIForm.Designer.cs
+++ WinForms/Forms/GDIForm.Designer.cs
@@ -30,24 +30,130 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelMissed = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelClock = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelBalls = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
             // 
-            this.timer1.Interval = 20;
+            this.timer1.Interval = 40;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.labelBalls);
+            this.panel1.Controls.Add(this.labelMissed);
+            this.panel1.Controls.Add(this.labelScore);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelClock);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelMissed
+            // 
+            this.labelMissed.AutoSize = true;
+            this.labelMissed.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelMissed.Location = new System.Drawing.Point(453, 9);
+            this.labelMissed.Name = "labelMissed";
+            this.labelMissed.Size = new System.Drawing.Size(37, 45);
+            this.labelMissed.TabIndex = 2;
+            this.labelMissed.Text = "0";
+            this.labelMissed.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelScore.Location = new System.Drawing.Point(252, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(37, 45);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "0";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(319, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Missed:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(140, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelClock.Location = new System.Drawing.Point(3, 9);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(136, 45);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "00:00:00";
+            this.labelClock.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(505, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 45);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Balls:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelBalls
+            // 
+            this.labelBalls.AutoSize = true;
+            this.labelBalls.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelBalls.Location = new System.Drawing.Point(602, 9);
+            this.labelBalls.Name = "labelBalls";
+            this.labelBalls.Size = new System.Drawing.Size(37, 45);
+            this.labelBalls.TabIndex = 2;
+            this.labelBalls.Text = "0";
+            this.labelBalls.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // GDIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(670, 456);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Name = "GDIForm";
             this.Text = "GDIForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GDIForm_FormClosing);
             this.Load += new System.EventHandler(this.GDIForm_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.GDIForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GDIForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GDIForm_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +161,13 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Label labelMissed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelBalls;
+        private System.Windows.Forms.Label label3;
     }
 }
--- WinForms/Forms/GDIForm.cs
+++ WinForms/Forms/GDIForm.cs
@@ -23,53 +23,143 @@ namespace WinForms.Forms
         static extern UInt32 TimeSetEvent(UInt32 uDelay, UInt32 uResolution, TimerCallback lpTimeProc, ref UInt32 dwUser, UInt32 eventType);
         [DllImport("winmm.dll", SetLastError = true, EntryPoint = "timeKillEvent")]
         static extern void TimeKillEvent(UInt32 uTimerId);
-        const int TIME_ONESHOT = 0;
-        const int TIME_PERIODIC = 1;
-        private uint uDelay, uResolution;
-        private UInt32 timerId;
-        UInt32 dwUser = 0;
+        const int TIME_ONESHOT = 0;                     // timer start once
+        const int TIME_PERIODIC = 1;                    // timer start periodic
+        private uint uDelay, uResolution;               // delay, the error
+        private UInt32 timerId;                         // id for timer
+        private UInt32 dwUser = 0;
+        private TimerCallback timerCallback = null!;    // delegate for timer
+        private GCHandle timerHandle;                   // gc handle for timer
+        private int ticks;                              // counter of ticks
+
+        // processing timer tick
         void Timer_Tick(UInt32 id, UInt32 msg, ref UInt32 userCtx, UInt32 rsv1, UInt32 rsv2)
         {
-                Invoke((Action)IncLabel);
-                timerId = TimeSetEvent(uDelay, uResolution, Timer_Tick, ref dwUser, TIME_ONESHOT);
+            try
+            {
+                if (!this.IsDisposed)
+                {
+                    this.Invoke((Action)IncLabel);
+                }
+            }
+            catch { }
         }
 
-
+        // painting timer label
         void IncLabel()
         {
-            if(!this.IsDisposed) this.Text = (int.Parse(this.Text) + 1).ToString();
+            if (!this.IsDisposed)
+            {
+                // if balls 0 - lose
+                if(ballsCount == 0)
+                {
+                    StopMmTimer();
+                    timer1.Stop();
+                    MessageBox.Show("Game over", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                ticks++;
+                int h = ticks / 3600;
+                int m = (ticks / 3600) / 60;
+                int s = ticks % 60;
+                // spawn balls for every 8 second
+                if(s % 8 == 0)
+                {
+                    ballsCount++;
+                    balls.Add(new Ball
+                    {
+                        X = _random.Next(this.Width),
+                        Y = _random.Next(this.Height),
+                        W = 20,
+                        H = 20,
+                        Vx = _random.Next(10) - 5,
+                        Vy = _random.Next(10) - 5,
+                        clearBrush = new SolidBrush(this.BackColor),
+                        clearPen = new Pen(this.BackColor, 3),
+                        ballPen = new Pen(Color.FromArgb(_random.Next(156), _random.Next(156), _random.Next(156)), 3),
+                        ballBrush = new SolidBrush(Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256)))
+                    });
+                    labelBalls.Text = ballsCount.ToString();
+                }
+                String hours = h.ToString("00");
+                String min = m.ToString("00");
+                String sec = s.ToString("00");
+                labelClock.Text = $"{hours}:{min}:{sec}";
+                // labelClock.Text = (int.Parse(this.Text) + 1).ToString();
+            }
+        }
+
+        // start multimedia timer
+        void StartMmTimer()
+        {
+            this.Text = "1";
+            uDelay = 1000;                                  // delay for 1 sec 
+            uResolution = 10;                               // the error
+            timerCallback = new TimerCallback(Timer_Tick);  // delegate for timer
+            timerHandle = GCHandle.Alloc(timerCallback);    // garbage collector handle for timer
+
+            timerId = TimeSetEvent(uDelay, uResolution, timerCallback, ref dwUser, TIME_PERIODIC);
+        }
+
+        // stop multimedia timer
+        void StopMmTimer()
+        {
+            TimeKillEvent(timerId);
+            timerHandle.Free();
         }
 
         #endregion
 
-        Ball ball;
-        static int formHeight;
-        static int formWidth;
-        public GDIForm()
+        private readonly Random _random;    // variable for random
+        private List<Ball> balls = new();   // list of balls
+        private static int formHeight;      // form height
+        private static int formWidth;       // form width
+        private static GDIForm? parent;     // parent window object
+        private RocketMoveDirections rocketMove = RocketMoveDirections.None;
+        private Rocket rocket;              // object for rocket
+        private Brush clearBrush;           // brush for cleaning
+        private int missed;                 // missed balls count
+        private int score;                  // score by pushed balls
+        private int ballsCount;             // how many balls on form
+        public GDIForm(Random random)
         {
+            this._random = random;
             InitializeComponent();
-            ball = new Ball
+            ballsCount = 0;
+            clearBrush = new SolidBrush(this.BackColor);
+            parent = this;
+            // cycle for adding 3 balls
+            for (int i = 0; i < 3; i++)
+            {
+                ballsCount++;
+                balls.Add(new Ball
+                {
+                    X = _random.Next(this.Width),
+                    Y = _random.Next(this.Height),
+                    W = 20,
+                    H = 20,
+                    Vx = _random.Next(10) - 5,
+                    Vy = _random.Next(10) - 5,
+                    clearBrush = new SolidBrush(this.BackColor),
+                    clearPen = new Pen(this.BackColor, 3),
+                    ballPen = new Pen(Color.FromArgb(_random.Next(156), _random.Next(156), _random.Next(156)), 3),
+                    ballBrush = new SolidBrush(Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256)))
+                });
+            }
+            // creating rocket
+            rocket = new Rocket
             {
                 X = this.Width / 2,
-                Y = this.Height / 2,
-                W = 20,
-                H = 20,
-                Vx = -2,
-                Vy = -2,
-                clearBrush = new SolidBrush(this.BackColor),
-                clearPen = new Pen(this.BackColor, 3),
-                ballPen = new Pen(Color.Tomato, 3),
-                ballBrush = new SolidBrush(Color.Tomato)
-            };  
+                W = 100,
+                H = 15,
+                Brush = new SolidBrush(Color.Tomato)
+            };
+            // count of balls
+            labelBalls.Text = ballsCount.ToString();
         }
         private void GDIForm_Load(object sender, EventArgs e)
         {
-            
-            /*this.Text = "1";
-            uDelay = 10;
-            uResolution = 10;
-            timerId = TimeSetEvent(uDelay, uResolution, Timer_Tick, ref dwUser, TIME_ONESHOT);*/
             timer1.Start();
+            StartMmTimer();
         }
 
         /// <summary>
@@ -77,8 +167,8 @@ namespace WinForms.Forms
         /// </summary>
         private void GDIForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            // TimeKillEvent(timerId);
             timer1.Stop();
+            StopMmTimer();
         }
 
         /// <summary>
@@ -88,11 +178,76 @@ namespace WinForms.Forms
         {
             formHeight = this.ClientSize.Height;
             formWidth = this.ClientSize.Width;
-            e.Graphics.DrawEllipse(ball.clearPen, ball.X, ball.Y, ball.W, ball.H);
-            // e.Graphics.FillEllipse(clearBrush, ball.X, ball.Y, ball.W, ball.H);
-            ball.Track();
-            e.Graphics.DrawEllipse(ball.ballPen, ball.X, ball.Y, ball.W, ball.H);
-            // e.Graphics.FillEllipse(ballBrush, ball.Y, ball.Y, ball.W, ball.H);
+            foreach (var ball in balls)
+            {
+                e.Graphics.DrawEllipse(ball.clearPen, ball.X, ball.Y, ball.W, ball.H);
+                ball.Track();
+                e.Graphics.DrawEllipse(ball.ballPen, ball.X, ball.Y, ball.W, ball.H);
+            }
+
+            // Rocket
+            // if key hold then move
+            switch (rocketMove)
+            {
+                case RocketMoveDirections.Left:
+                    e.Graphics.FillRectangle(clearBrush,
+                       rocket.X,
+                       this.ClientSize.Height - rocket.H,
+                       rocket.W,
+                       rocket.H);
+                    rocket.V = -10;
+                    rocket.Track();
+                    break;
+                case RocketMoveDirections.Right:
+                    e.Graphics.FillRectangle(clearBrush,
+                       rocket.X,
+                       this.ClientSize.Height - rocket.H,
+                       rocket.W,
+                       rocket.H);
+                    rocket.V = 10;
+                    rocket.Track();
+                    break;
+                case RocketMoveDirections.None:
+                    break;
+            }
+            e.Graphics.FillRectangle(rocket.Brush,
+                rocket.X,this.ClientSize.Height - rocket.H,
+                rocket.W,rocket.H);
+
+            // Rocket ball collision
+            Ball toRemove = null!;
+            foreach (var ball in balls)
+            {
+                if(ball.Y >= this.ClientSize.Height)
+                {
+                    toRemove = ball;
+                    
+                }
+                else if(ball.Y + ball.H >= this.ClientSize.Height - rocket.H
+                    && ball.Y + ball.H <= this.ClientSize.Height)
+                {
+                    if(ball.X + ball.W / 2 >= rocket.X && ball.X + ball.W / 2 <= 
+                        rocket.X + rocket.W)
+                    {
+                        // Back
+                        score++;
+                        labelScore.Text = score.ToString();
+                        ball.Vy = -ball.Vy;
+                        ball.Y = this.ClientSize.Height - rocket.H - ball.H;
+                    }
+                    
+                }
+            }
+            // removing ball
+            if (toRemove != null)
+            {
+                ballsCount--;
+                balls.Remove(toRemove);
+                missed++;
+                labelMissed.Text = missed.ToString();
+                labelBalls.Text = ballsCount.ToString();
+            }
+
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -132,7 +287,7 @@ namespace WinForms.Forms
                     X = 0;
                     Vx = -Vx;
                 }
-                else if (X + W > GDIForm.formWidth) // left frame
+                else if (X + W >= GDIForm.formWidth) // left frame
                 {
                     X = GDIForm.formWidth - W;
                     Vx = -Vx;
@@ -142,12 +297,69 @@ namespace WinForms.Forms
                     Y = 0;
                     Vy = -Vy;
                 }
-                else if (Y + H > GDIForm.formHeight) // bottom frame
+               /* else if (Y + H >= GDIForm.formHeight) // bottom frame
                 {
                     Y = GDIForm.formHeight - H;
                     Vy = -Vy;
+                }*/
+            }
+        }
+
+        // class for rocket
+        class Rocket
+        {
+            public int X { get; set; }          // X position
+            public int W { get; set; }          // width
+            public int H { get; set; }          // height
+            public int V { get; set; }          // speed
+            public Brush Brush { get; set; }    // brush for painting
+
+            // method for moving
+            public void Track()
+            {
+                X += V;
+                if (X < 0) // right frame
+                {
+                    X = 0;
+                    V = -V;
                 }
-            }  
+                if (X + W >= GDIForm.formWidth) // left frame
+                {
+                    X = GDIForm.formWidth - W;
+                    V = -V;
+                }
+            }
+        }
+
+        private void GDIForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.A)
+            {
+                rocketMove = RocketMoveDirections.Left;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                rocketMove = RocketMoveDirections.Right;
+            }
+        }
+
+        private void GDIForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && rocketMove == RocketMoveDirections.Left)
+            {
+                rocketMove = RocketMoveDirections.None;
+            }
+            if (e.KeyCode == Keys.D && rocketMove == RocketMoveDirections.Right)
+            {
+                rocketMove = RocketMoveDirections.None;
+            }
+        }
+
+        enum RocketMoveDirections
+        {
+            None,
+            Left,
+            Right
         }
     }
 }
