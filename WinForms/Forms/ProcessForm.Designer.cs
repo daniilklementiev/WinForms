33c3f8586f81e0e122f4b965d8b22e3ff845622a
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-04-16 13:01:02 +0300

    + updating + link to itstep.org + link to od.itstep.org + comments

--- WinForms/Forms/ProcessForm.Designer.cs
+++ WinForms/Forms/ProcessForm.Designer.cs
@@ -28,25 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.linkLabelOdItStep = new System.Windows.Forms.LinkLabel();
+            this.linkLabelItStep = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonBrowserStop = new System.Windows.Forms.Button();
+            this.buttonBrowserStart = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.buttonNotepadLog = new System.Windows.Forms.Button();
             this.buttonNotepadStop = new System.Windows.Forms.Button();
             this.buttonNotepadStart = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.treeViewProcesses = new System.Windows.Forms.TreeView();
+            this.timerRefresher = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.linkLabelOdItStep);
+            this.groupBox1.Controls.Add(this.linkLabelItStep);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.groupBox4);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 448);
+            this.groupBox1.Size = new System.Drawing.Size(400, 640);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Дочерние процессы";
+            // 
+            // linkLabelOdItStep
+            // 
+            this.linkLabelOdItStep.AutoSize = true;
+            this.linkLabelOdItStep.LinkVisited = true;
+            this.linkLabelOdItStep.Location = new System.Drawing.Point(53, 534);
+            this.linkLabelOdItStep.Name = "linkLabelOdItStep";
+            this.linkLabelOdItStep.Size = new System.Drawing.Size(224, 32);
+            this.linkLabelOdItStep.TabIndex = 1;
+            this.linkLabelOdItStep.TabStop = true;
+            this.linkLabelOdItStep.Text = "https://od.itstep.org";
+            this.linkLabelOdItStep.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelOdItStep_LinkClicked);
+            // 
+            // linkLabelItStep
+            // 
+            this.linkLabelItStep.AutoSize = true;
+            this.linkLabelItStep.LinkVisited = true;
+            this.linkLabelItStep.Location = new System.Drawing.Point(53, 490);
+            this.linkLabelItStep.Name = "linkLabelItStep";
+            this.linkLabelItStep.Size = new System.Drawing.Size(191, 32);
+            this.linkLabelItStep.TabIndex = 1;
+            this.linkLabelItStep.TabStop = true;
+            this.linkLabelItStep.Text = "https://itstep.org";
+            this.linkLabelItStep.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelItStep_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Location = new System.Drawing.Point(53, 446);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(276, 32);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://mystat.itstep.org/";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.buttonBrowserStop);
+            this.groupBox4.Controls.Add(this.buttonBrowserStart);
+            this.groupBox4.Location = new System.Drawing.Point(97, 248);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(175, 183);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Браузер";
+            // 
+            // buttonBrowserStop
+            // 
+            this.buttonBrowserStop.Location = new System.Drawing.Point(21, 84);
+            this.buttonBrowserStop.Name = "buttonBrowserStop";
+            this.buttonBrowserStop.Size = new System.Drawing.Size(126, 40);
+            this.buttonBrowserStop.TabIndex = 1;
+            this.buttonBrowserStop.Text = "Стоп";
+            this.buttonBrowserStop.UseVisualStyleBackColor = true;
+            this.buttonBrowserStop.Click += new System.EventHandler(this.buttonBrowserStop_Click);
+            // 
+            // buttonBrowserStart
+            // 
+            this.buttonBrowserStart.Location = new System.Drawing.Point(21, 38);
+            this.buttonBrowserStart.Name = "buttonBrowserStart";
+            this.buttonBrowserStart.Size = new System.Drawing.Size(126, 40);
+            this.buttonBrowserStart.TabIndex = 1;
+            this.buttonBrowserStart.Text = "Пуск";
+            this.buttonBrowserStart.UseVisualStyleBackColor = true;
+            this.buttonBrowserStart.Click += new System.EventHandler(this.buttonBrowserStart_Click);
             // 
             // groupBox2
             // 
@@ -98,18 +183,66 @@
             this.label1.Size = new System.Drawing.Size(0, 32);
             this.label1.TabIndex = 0;
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelTimer);
+            this.groupBox3.Controls.Add(this.buttonRefresh);
+            this.groupBox3.Controls.Add(this.treeViewProcesses);
+            this.groupBox3.Location = new System.Drawing.Point(458, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(860, 628);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Системные процессы";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Font = new System.Drawing.Font("Segoe UI", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTimer.Location = new System.Drawing.Point(19, 156);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(156, 64);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "00:00:00";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(59, 64);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(150, 46);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "Обновить";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // treeViewProcesses
+            // 
+            this.treeViewProcesses.Location = new System.Drawing.Point(247, 38);
+            this.treeViewProcesses.Name = "treeViewProcesses";
+            this.treeViewProcesses.Size = new System.Drawing.Size(588, 560);
+            this.treeViewProcesses.TabIndex = 0;
+            this.treeViewProcesses.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeViewProcesses_NodeMouseClick);
+            // 
+            // timerRefresher
+            // 
+            this.timerRefresher.Tick += new System.EventHandler(this.timerRefresher_Tick);
+            // 
             // ProcessForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 464);
+            this.ClientSize = new System.Drawing.Size(1330, 638);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Name = "ProcessForm";
             this.Text = "ProcessForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProcessForm_FormClosing);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,5 +256,16 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button buttonNotepadLog;
         private System.Windows.Forms.Button buttonNotepadStop;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TreeView treeViewProcesses;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Timer timerRefresher;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonBrowserStop;
+        private System.Windows.Forms.Button buttonBrowserStart;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabelOdItStep;
+        private System.Windows.Forms.LinkLabel linkLabelItStep;
     }
 }
--- WinForms/Forms/ProcessForm.cs
+++ WinForms/Forms/ProcessForm.cs
@@ -15,11 +15,14 @@ namespace WinForms.Forms
     {
         private List<Process> processes;    // list of started processes
         private bool isStart;               // bool var that toggled when proc start
+        private int ticks;                  // ticks for timer
         public ProcessForm()
         {
+            ticks = 0;
             isStart = false;
             processes = new();
             InitializeComponent();
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
         }
 
         private void buttonNotepadStart_Click(object sender, EventArgs e)
@@ -46,8 +49,8 @@ namespace WinForms.Forms
                 }
                 isStart = false;  // toggling var to false
             }
-            
-            
+
+
         }
         private void buttonNotepadLog_Click(object sender, EventArgs e)
         {
@@ -57,7 +60,7 @@ namespace WinForms.Forms
                 processes.Add(Process.Start("notepad.exe", /*starting proc notepad*/
                     Application.StartupPath + "log.txt")); // and opening log.txt in \bin
                 isStart = true;
-            } 
+            }
             else MessageBox.Show("Process already started");
         }
 
@@ -70,6 +73,138 @@ namespace WinForms.Forms
                 proc.Dispose();
             }
             isStart = false;
+            timerRefresher.Stop();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            // foreach(var process in processes.OrderBy(p => p.ProcessName))
+            // {
+            //     // creating object-node of "tree"
+            //     TreeNode node = new TreeNode();
+            //     // configuring node
+            //     node.Text = process.ProcessName;
+            // 
+            //     // adding node to tree
+            //     treeViewProcesses.Nodes.Add(node);
+            // }
+            timerRefresher.Start();
+            foreach (var grp in processes.GroupBy(p => p.ProcessName,
+                (n, g) => new { Name = n, Processes = g.ToList() }).OrderBy(grp => grp.Name))
+            {
+                TreeNode node = new TreeNode();
+                node.Text = $"{grp.Name} | {grp.Processes.Count}";
+
+                foreach (var proc in grp.Processes)
+                {
+                    TreeNode subNode = new TreeNode();
+                    subNode.Text = $"Proc #{proc.Id} | Prior \"{proc.BasePriority}\" | ({proc.Threads.Count})";
+                    foreach (ProcessThread thread in proc.Threads)
+                    {
+                        subNode.Nodes.Add(new TreeNode($"Thread #{thread.Id} | Prior \"{proc.BasePriority}\""));
+                    }
+                    subNode.Tag = proc;
+                    node.Nodes.Add(subNode);
+                }
+                node.BackColor = Color.LightGreen;
+                treeViewProcesses.Nodes.Add(node);
+            }
+        }
+
+        private void treeViewProcesses_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (MessageBox.Show("Kill?", "Confirm", MessageBoxButtons.YesNo)
+                    == DialogResult.Yes)
+                {
+                    ((Process)e.Node.Tag)?.Kill();
+                }
+            }
+        }
+
+        private void timerRefresher_Tick(object sender, EventArgs e)
+        {
+            // show timer
+            ticks++;
+            int intHours = ticks / 3600;
+            int intMinutes = (ticks / 3600) / 60;
+            int intSec = ticks % 60;
+            String hours = intHours.ToString("00");
+            String min = intMinutes.ToString("00");
+            String sec = intSec.ToString("00");
+            labelTimer.Text = $"{hours}:{min}:{sec}";
+            
+            // get all processes
+            Process[] processes = Process.GetProcesses();
+
+            // checking each processor
+            for (int i = 0; i < processes.Length; i++)
+            {
+                TreeNodeCollection nodes = treeViewProcesses.Nodes;  // collection of nodes in threeView
+                bool isContain = false;                              // bool var for contain
+
+                for (int j = 0; j < nodes.Count; j++)
+                {
+                    // Does such a node already exist 
+                    if (nodes[j].Text.Contains(processes[i].ProcessName))
+                    {
+                        isContain = true;
+                    }
+                }
+
+                // if node doesn't exist - creating new node
+                if (!isContain)
+                {
+                    TreeNode newNode = new TreeNode(processes[i].ProcessName);
+                    newNode.BackColor = Color.Yellow;
+                    treeViewProcesses.Nodes.Add(newNode);
+
+                }
+            }
+        }
+
+        private void buttonBrowserStart_Click(object sender, EventArgs e)
+        {
+            if (!isStart)
+            {
+                processes.Add(Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"));  // starting proc
+                isStart = true;                               // toggle var to true
+            }
+            else MessageBox.Show("Process already started");
+        }
+
+        private void buttonBrowserStop_Click(object sender, EventArgs e)
+        {
+            // stopping all processes
+            foreach (var proc in processes)
+            {
+                proc.Kill();
+                proc.Dispose();
+            }
+            isStart = false;  // toggling var to false
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            processes.Add(Process.Start(
+                    @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                                                       "https://mystat.itstep.org/"));
+        }
+
+        private void linkLabelOdItStep_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            processes.Add(Process.Start(
+                    @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                                                       "https://od.itstep.org/"));
+        }
+
+        private void linkLabelItStep_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            processes.Add(Process.Start(
+                    @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                                                       "https://itstep.org/"));
         }
     }
 }
